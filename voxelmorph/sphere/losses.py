b74cb9ac734866fef2787008cb083261ac911c28
Kid-sunzhenyu <zhenyu.sun@neuralgalaxy.com>
2021-09-16 17:51:19 +0800

    更改为2D时的损失函数

--- voxelmorph/sphere/losses.py
+++ voxelmorph/sphere/losses.py
@@ -100,17 +100,21 @@ class Grad:
         self.loss_mult = loss_mult
 
     def loss(self, _, y_pred):
-        dy = torch.abs(y_pred[:, :, 1:, :, :] - y_pred[:, :, :-1, :, :])
-        dx = torch.abs(y_pred[:, :, :, 1:, :] - y_pred[:, :, :, :-1, :])
-        dz = torch.abs(y_pred[:, :, :, :, 1:] - y_pred[:, :, :, :, :-1])
+        # dy = torch.abs(y_pred[:, :, 1:, :, :] - y_pred[:, :, :-1, :, :])
+        # dx = torch.abs(y_pred[:, :, :, 1:, :] - y_pred[:, :, :, :-1, :])
+        # dz = torch.abs(y_pred[:, :, :, :, 1:] - y_pred[:, :, :, :, :-1])
+        dy = torch.abs(y_pred[:, :, 1:, :] - y_pred[:, :, :-1, :])
+        dx = torch.abs(y_pred[:, :, :, 1:] - y_pred[:, :, :, :-1])
+
 
         if self.penalty == 'l2':
             dy = dy * dy
             dx = dx * dx
-            dz = dz * dz
+            # dz = dz * dz
 
-        d = torch.mean(dx) + torch.mean(dy) + torch.mean(dz)
-        grad = d / 3.0
+        # d = torch.mean(dx) + torch.mean(dy) + torch.mean(dz)
+        d = torch.mean(dx) + torch.mean(dy)
+        grad = d / 2.0
 
         if self.loss_mult is not None:
             grad *= self.loss_mult
