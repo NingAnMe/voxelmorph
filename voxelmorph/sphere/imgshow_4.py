3b38530066cdd6f6cdb56a8c0e066e21d428712d
Kid-sunzhenyu <zhenyu.sun@neuralgalaxy.com>
2021-09-23 09:45:50 +0800

    调整图像位置

--- voxelmorph/sphere/imgshow_4.py
+++ voxelmorph/sphere/imgshow_4.py
@@ -6,10 +6,12 @@ from astropy.coordinates import cartesian_to_spherical, spherical_to_cartesian
 import pylab
 import math
 
+
 def load_file_npz(input_npz):
     img_temp = io.imread(input_npz)
     img = np.array(img_temp)
     return img
+
 
 def load_file_tif(input_tif):
     data = np.zeros((512, 256), dtype=np.float)
@@ -21,8 +23,8 @@ def load_file_tif(input_tif):
 
     return data
 
-def load_file_sphere(lh_sphere, lh_sulc):
 
+def load_file_sphere(lh_sphere, lh_sulc):
     coords, faces = nib.freesurfer.read_geometry(lh_sphere)
 
     r, phi, theta = cartesian_to_spherical(coords[:, 0], coords[:, 1], coords[:, 2])
@@ -46,8 +48,8 @@ def load_file_sphere(lh_sphere, lh_sulc):
 
     return values
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--moving', required=True, help='moving image (source) filename')
     parser.add_argument('--fixed', required=True, help='fixed image (target) filename')
@@ -69,35 +71,24 @@ if __name__ == '__main__':
     # moved_freesurfer_sphere = '/home/zhenyu/SurfReg/freesurfer_reg/lh.sphere.reg'
     # moved_freesurfer_sulc = '/home/zhenyu/SurfReg/freesurfer_reg/lh.sulc'
 
-    plt.figure(figsize=(16, 8))
+    fig = plt.figure(figsize=(36, 9))
+    ax = fig.add_subplot(1, 4, 1)
     moving_img = load_file_npz(moving)
     plt.title("moving")
     plt.imshow(moving_img)
 
-    plt.figure(figsize=(16, 8))
+    ax = fig.add_subplot(1, 4, 2)
     fixed_img = load_file_npz(fixed)
     plt.title("fixed")
     plt.imshow(fixed_img)
 
-    plt.figure(figsize=(16, 8))
+    ax = fig.add_subplot(1, 4, 3)
     moved_img = load_file_npz(moved_model)
     plt.title("moved_model")
     plt.imshow(moved_img)
 
-
-    plt.figure(figsize=(16, 8))
+    ax = fig.add_subplot(1, 4, 4)
     moved_freesurfer_img = load_file_sphere(moved_freesurfer_sphere, moved_freesurfer_sulc)
     plt.title("moved_freesurfer")
-    plt.imshow(fixed_img)
-    pylab.show()
-
-
-
-
-
-
-
-
-
-
+    plt.imshow(moved_freesurfer_img)
 
